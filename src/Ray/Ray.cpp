ee62a5205378c5a161b01211e43c7ea9a0c68540
mollnn <1084918073@qq.com>
2022-05-09 21:48:58 +0800

    fix: compile error in ray, scene

---
+++ src/Ray/Ray.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file Ray.cpp
+ * @author Zhimin Fan
+ * @brief Ray implemention.
+ * @version 0.1
+ * @date 2022-05-09
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "../Core/FunctionLayer/Ray.h"
+
+Ray::Ray(const Point3f &_origin, const Vec3f &_direction, double _timeMin, double _timeMax)
+{
+    // todo
+}
---
+++ src/Scene/Scene.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file Scene.cpp
+ * @author Zhimin Fan
+ * @brief Ray implemention.
+ * @version 0.1
+ * @date 2022-05-09
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "../Core/FunctionLayer/Scene.h"
+
+Intersection Scene::intersect(const Ray &r) const
+{
+    // todo
+    return Intersection();
+}
