9564f966d00f5020f6d0022fb5b2638f9dbe9290
ChenWang <1401520906@qq.com>
2022-05-18 17:19:44 +0800

    fix: `RunSelfTest.bat` cmake build path

--- RunSelfTest.bat
+++ RunSelfTest.bat
@@ -4,7 +4,7 @@ set cur_dir=%~dp0
 set test_dir=%cur_dir%SelfTest\
 set target_dir=%test_dir%target\Debug\
 
-cmake %test_dir%build\.. %test_dir%build\.
+cmake %test_dir%build\.. -B %test_dir%build\.
 cmake --build %test_dir%build\.
 
-@REM start %target_dir%SelfTest.exe
+%target_dir%SelfTest.exe
