ae4be0de2906ce4162b0bdd2193a245c3ba873ec
mollnn <1084918073@qq.com>
2022-06-18 16:00:58 +0800

    test (integrator): new test case for validation

--- test/test-integrator-validate/Main.cpp
+++ test/test-integrator-validate/Main.cpp
@@ -32,7 +32,8 @@
 #include "../src/Material/MatteMaterial.h"
 #include "../src/Material/TestMirror.h"
 
-TEST_CASE("test-integrator")
+
+TEST_CASE("test-integrator-validate")
 {
     Spectrum::init();
     std::cout << "NJUCG Zero v0.1" << std::endl;
@@ -40,14 +41,8 @@ TEST_CASE("test-integrator")
     std::cout << "scene start" << std::endl;
     std::shared_ptr<ImageTexture<Spectrum, RGB3>> imageTexture = std::make_shared<ImageTexture<Spectrum, RGB3>>("../asset/tex.jpg");
     std::shared_ptr<MatteMaterial> lambert = std::make_shared<MatteMaterial>(std::make_shared<ConstantTexture<Spectrum>>(RGB3(0.5, 0.5, 0.5).toSpectrum()));
-    std::shared_ptr<MatteMaterial> lambertR = std::make_shared<MatteMaterial>(std::make_shared<ConstantTexture<Spectrum>>(RGB3(0.8, 0.0, 0.0).toSpectrum()));
-    std::shared_ptr<MatteMaterial> lambertG = std::make_shared<MatteMaterial>(imageTexture);
-    std::shared_ptr<MatteMaterial> lambertB = std::make_shared<MatteMaterial>(std::make_shared<ConstantTexture<Spectrum>>(RGB3(0.0, 0.0, 0.8).toSpectrum()));
     std::shared_ptr<TestMirror> mirror = std::make_shared<TestMirror>();
-    scene->addEntity(std::make_shared<Sphere>(Point3d(0.0, -1.5, 1.0), 1.0, mirror));
-    scene->addEntity(std::make_shared<Sphere>(Point3d(0.0, 0.0, -1.0), 1.0, lambertG));
-    scene->addEntity(std::make_shared<Sphere>(Point3d(2.1, 0.0, -2.0), 1.0, lambertR));
-    scene->addEntity(std::make_shared<Sphere>(Point3d(-2.1, 0.0, -2.0), 1.0, lambertB));
+    scene->addEntity(std::make_shared<Sphere>(Point3d(0.0, 0.0, -1.0), 1.0, lambert));
     scene->addEntity(std::make_shared<Sphere>(Point3d(0.0, -101.0, 0.0), 100.0, lambert));
     std::cout << "scene created" << std::endl;
     scene->addLight(std::make_shared<PointLight>(32.0, Point3d(0, 3, 1)));
@@ -64,8 +59,7 @@ TEST_CASE("test-integrator")
     PathIntegrator integrator(thinlens, std::make_unique<Film>(Point2i(128, 128), 3), nullptr, std::make_shared<DirectSampler>(), 16);
     std::cout << "start rendering" << std::endl;
     integrator.render(scene);
-    integrator.save("result-thinlens2.bmp");
+    integrator.save("result-integrator-validate.bmp");
     std::cout << "finish" << std::endl;
     return;
 }
-
