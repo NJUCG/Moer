21e79849e51bf596290b1cbd37f2b770518af27e
xiaoyaoing <1924188282@qq.com>
2022-06-22 21:25:13 +0800

    chore () : savechanges

--- src/Material/MatteMaterial.cpp
+++ src/Material/MatteMaterial.cpp
@@ -5,7 +5,7 @@ std::shared_ptr<BxDF> MatteMaterial::getBxDF(Intersection intersect) const
 {
     auto color = kd->eval(intersect);
     std::shared_ptr<Diffuse> bxdf = std::make_shared<Diffuse>(color);
-    return std::make_shared<Mircofacet>(color,1.5046,1.000277,0.05);
+//    return std::make_shared<Mircofacet>(color,1.5046,1.000277,0.05);
     return bxdf;
 }
 std::shared_ptr<BSSRDF> MatteMaterial::getBSSRDF(Intersection intersect) const
