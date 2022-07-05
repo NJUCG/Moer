21fc62dc3953bbaf56ca7d0d661b0a1fba31eab0
xiaoyaoing <1924188282@qq.com>
2022-07-05 21:39:11 +0800

    chore():delete build files

--- cmake-build-debug/ext/assimp/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/code/Zero/ext/assimp/contrib/zlib
-# Build directory: C:/code/Zero/cmake-build-debug/ext/assimp/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
