c4f3e341c216b034b9153281817e5d245616730b
ChenxiZhou0619 <1305845549@qq.com>
2022-09-12 19:01:30 +0800

    before fix bvh

--- src/FunctionLayer/Shape/Mesh.cpp
+++ src/FunctionLayer/Shape/Mesh.cpp
@@ -2,12 +2,17 @@
 #include "FunctionLayer/Intersection.h"
 
 Mesh::Mesh(std::shared_ptr<MeshData> _data, 
-           std::shared_ptr<Material> _material)
-    : data(_data)
-{
+           std::shared_ptr<Material> _material): 
+    m_vertices(_data->m_vertices),
+    m_normals(_data->m_normals),
+    m_tangents(_data->m_tangents),
+    m_bitangents(_data->m_bitangents),
+    m_UVs(_data->m_UVs),
+    m_indices(_data->m_indices)
+{   
     this->material = _material;
-
-    //* Build bvh for the mesh
+/*
+    // Build bvh for the mesh
     m_triangles.reserve(data->getTriangleNum());
     for (int i = 0; i < data->getTriangleNum(); ++i) {
         std::vector<Point3d> vertices;
@@ -25,35 +30,27 @@ Mesh::Mesh(std::shared_ptr<MeshData> _data,
     }
 
     BVH = std::make_shared<Bvh>(m_triangles);
+*/
+    auto maxX = m_vertices.row(0).maxCoeff(),
+         minX = m_vertices.row(0).minCoeff(),
+         maxY = m_vertices.row(1).maxCoeff(),
+         minY = m_vertices.row(1).minCoeff(),
+         maxZ = m_vertices.row(2).maxCoeff(),
+         minZ = m_vertices.row(2).minCoeff();
+
+    m_aabb = BoundingBox3f {
+        Point3d {minX, minY, minZ},
+        Point3d {maxX, maxY, maxZ}
+    };
+
+    
+
+
 }
 
 std::optional<Intersection> Mesh::intersect(const Ray &r) const
 {
-    if(BVH)
-        return BVH->Intersect(r);
-    
-    std::optional<Intersection> result = std::nullopt;
-    for (int i = 0; i < data->getTriangleNum(); ++i) {
-        std::vector<Point3d> vertices;
-        auto indices = data->getTriangleIndiceAt(i);
-        vertices.emplace_back(data->getVertexAt(indices[0]));
-        vertices.emplace_back(data->getVertexAt(indices[1]));
-        vertices.emplace_back(data->getVertexAt(indices[2]));
-        Triangle triangle {vertices, this->material};
-
-        auto intersect = triangle.intersect(r);
-        if (intersect.has_value()) {
-            if (!result.has_value())
-                result = intersect;
-            else {
-                if (intersect->t < result->t)
-                    result = intersect;
-            }
-        }
-    }
-    if (result)
-        result.value().object = this;
-    return result;
+    return BVH->Intersect(r);
 }
 
 void Mesh::apply()
@@ -83,5 +80,5 @@ void Mesh::setLight(std::shared_ptr<Light> light)
 BoundingBox3f Mesh::WorldBound() const
 {
     // TODO, store the bounding box
-    return data->getAABB();
+    return m_aabb;
 }
