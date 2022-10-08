bd505e7f775ec0e61cc8ae25821b0c234b88baa0
ja50n <zs_feng@qq.com>
2022-10-08 23:48:18 +0800

    fix(FunctionLayer\Shape\Triangle.h): Placeholder of returning value.

--- src/FunctionLayer/Shape/Triangle.h
+++ src/FunctionLayer/Shape/Triangle.h
@@ -61,7 +61,10 @@ public:
 	virtual void setLight(std::shared_ptr<Light> light) override;
 	virtual BoundingBox3f WorldBound() const override;
 
-	virtual RTCGeometry toEmbreeGeometry(RTCDevice device) const override {}
+	virtual RTCGeometry toEmbreeGeometry(RTCDevice device) const override {
+    RTCGeometry geom;
+    return geom;
+  }
 	virtual EntitySurfaceInfo getEntitySurfaceInfo(int primID, Point2d uv) const override {
 		return EntitySurfaceInfo();
 	}
