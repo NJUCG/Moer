c33c2a5c6853ee00ecf6c4d208f04c4cdbd4020b
ja50n <zs_feng@qq.com>
2022-11-22 20:58:08 +0800

    test(): Tested Stratified and Halton sampler.

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,14 @@
 #include "FunctionLayer/Camera/Pinhole.h"
 #include "FunctionLayer/TileGenerator/SequenceTileGenerator.h"
 #include "FunctionLayer/Sampler/Independent.h"
+#include "FunctionLayer/Sampler/Stratified.h"
+#include "FunctionLayer/Sampler/Halton.h"
 
 struct Render{
 public:
    static  void RenderScene(const std::string sceneWorkingDir){
        FileUtils::setWorkingDir(sceneWorkingDir + "/");
-       Json  sceneJson;
+       Json sceneJson;
        std::ifstream sceneFile(FileUtils::getWorkingDir()+std::string("scene.json"));
        sceneFile>>sceneJson;
        std::shared_ptr<Scene> scene = std::make_shared<Scene>(sceneJson);
@@ -19,21 +21,27 @@ public:
        scene->build();
        std::cout << "scene prepared" << std::endl;
        //todo : config camera integrator,film,tile Generator,sampler by Json
-       Point3d lookFrom(3.04068,
-                        3.17153,
-                        3.20454),
-               lookAt(0.118789,
-                      0.473398,
-                      0.161081);
+       /// @note Do we need reflection in order to configure them in GUI?
+       int res_w = 600, res_h = 600;
+       Point3d lookFrom(0, 1, 4.5),
+               lookAt(0, 1, 0);
        Vec3d up(0, 1, 0);
        auto pinhole = std::make_shared<PinholeCamera>(
-               lookFrom, lookAt, up, 35, (float(1280)/float(780)), 3.17f);
+               lookFrom, lookAt, up, 35, (float(res_w)/float(res_h)), 3.17f);
 
-       PathIntegratorNew integrator(pinhole, std::make_unique<Film>(Point2i(1280,780), 3),
-                                    std::make_unique<SequenceTileGenerator>(Point2i(1280,780)), std::make_shared<IndependentSampler>(), 32, 12);
+       PathIntegratorNew integrator(
+        pinhole,                                                    // Camera
+        std::make_unique<Film>(Point2i(res_w, res_h), 3),               // Film
+        std::make_unique<SequenceTileGenerator>(Point2i(res_w, res_h)), // Tile
+        // std::make_shared<IndependentSampler>(),                     // Sampler
+        // std::make_shared<StratifiedSampler>(6, 10),
+        std::make_shared<HaltonSampler>(6, res_w, res_h, 10),
+        16,                                                         // Spp
+        12                                                          // nThread
+        );
        std::cout << "start rendering" << std::endl;
        integrator.render(scene);
-       integrator.save("testball");
+       integrator.save("cornell-box.halton");
        std::cout << "finish" << std::endl;
    }
 };
