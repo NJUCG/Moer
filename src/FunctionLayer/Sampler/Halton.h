3cb7245ca9720dc6698ebaba4f45a5538b99f1fe
Chenxi Zhou <100562961+ChenxiZhou0619@users.noreply.github.com>
2022-11-29 21:42:02 +0800

    fix: headerfiles problems in linux (#40)
    * fix headerfiles problems
    
    * change headerfile name

--- src/FunctionLayer/Sampler/Halton.h
+++ src/FunctionLayer/Sampler/Halton.h
@@ -15,41 +15,41 @@
 static constexpr int MAX_RESOLUTION = 128;
 
 class HaltonSampler : public GlobalSampler {
- public:
-  HaltonSampler(const HaltonSampler&) = default;
-  HaltonSampler(int64_t _sppSqrt, int resolution_w, int resolution_h);
-  HaltonSampler(int64_t _sppSqrt, int resolution_w, int resolution_h,
-                int _nDim);
-  virtual int64_t globalSampleIndex(int64_t pixelSampleIndex) const override;
-  virtual double sampleValue(int64_t globalIndex, int dim) const override;
-  virtual std::unique_ptr<Sampler> clone(int seed) const override;
+public:
+    HaltonSampler(const HaltonSampler &) = default;
+    HaltonSampler(int64_t _sppSqrt, int resolution_w, int resolution_h);
+    HaltonSampler(int64_t _sppSqrt, int resolution_w, int resolution_h,
+                  int _nDim);
+    virtual int64_t globalSampleIndex(int64_t pixelSampleIndex) const override;
+    virtual double sampleValue(int64_t globalIndex, int dim) const override;
+    virtual std::unique_ptr<Sampler> clone(int seed) const override;
 
- private:
-  /**
+private:
+    /**
    * @brief All share the same perms.
    */
-  static std::vector<uint16_t> scramblePermutations;
-  /**
+    static std::vector<uint16_t> scramblePermutations;
+    /**
    * @brief Value and power required to scale base-2 and base-3
    *        till no less than resoluton_w and resolution_h.
    *        A sample will be discarded if the first two dimensions
    *        exceeded the MAX_RESOLUTION or resolution_[wh] after scaling.
    */
-  Point2i scaleVal, scaleExp;
-  mutable Point2i pixelForOffset =
-      Point2i(std::numeric_limits<int>::max(), std::numeric_limits<int>::max());
-  mutable int64_t globalOffsetForPixelPosition;
-  int sampleStride;
-  Point2i multInverse;
+    Point2i scaleVal, scaleExp;
+    mutable Point2i pixelForOffset =
+        Point2i(std::numeric_limits<int>::max(), std::numeric_limits<int>::max());
+    mutable int64_t globalOffsetForPixelPosition;
+    int sampleStride;
+    Point2i multInverse;
 
-  /**
+    /**
    * @brief Beginning of the scramble permutation on dim-th dimension.
    *
    * @param dim Dimension index, starting by zero.
    * @return const uint16_t* Pointer to the first value of the permutation.
    */
-  const uint16_t *permutationOfDimension(int dim) const {
-    assert(0 <= dim && dim <= nPrimes);
-    return &(scramblePermutations[PrimeSums[dim]]);
-  }
+    const uint16_t *permutationOfDimension(int dim) const {
+        assert(0 <= dim && dim <= nPrimes);
+        return &(scramblePermutations[PrimeSums[dim]]);
+    }
 };
