aa2531e72896444149c9e0d0043f67527eda2e16
Chenxi Zhou <100562961+ChenxiZhou0619@users.noreply.github.com>
2022-11-30 17:47:20 +0800

    build: try to fix pthreads (#42)
    * fix headerfiles problems
    
    * change headerfile name
    
    * try to fix pthread

--- src/ResourceLayer/File/Image.cpp
+++ src/ResourceLayer/File/Image.cpp
@@ -9,42 +9,45 @@ Image::Image() {
     imageRawData = nullptr;
 }
 
-Image::~Image( ) {
-    delete[] imageRawData;
+Image::~Image() {
+    if (isHdr)
+        delete[] static_cast<float *>(imageRawData);
+    else
+        delete[] static_cast<unsigned char *>(imageRawData);
 }
 
-template < typename T >
-T * Image::as( ) const {
+template<typename T>
+T *Image::as() const {
     return reinterpret_cast<T *>(imageRawData);
 }
 
-Image::Image(const std::string & path, ImageLoadMode ilm) {
+Image::Image(const std::string &path, ImageLoadMode ilm) {
 
     // todo: support alpha reading
-	// todo: do gamma correction
+    // todo: do gamma correction
     isHdr = stbi_is_hdr(path.c_str());
     int w, h, c;
-    if ( isHdr ) {
-        float * data = stbi_loadf(path.c_str(), & w, & h, & c, 3);
+    if (isHdr) {
+        float *data = stbi_loadf(path.c_str(), &w, &h, &c, 3);
         imageRawData = new float[w * h * c];
         std::memcpy(imageRawData, data, w * h * c * sizeof(float));
         free(data);
     } else
-        // todo: bw mode
+    // todo: bw mode
     {
-        unsigned char * data = stbi_load(path.c_str(), & w, & h, & c, 3);
-        if ( ! data ) {
+        unsigned char *data = stbi_load(path.c_str(), &w, &h, &c, 3);
+        if (!data) {
             std::cout << "Fail to load image " << path << std::endl;
             return;
         }
         imageRawData = new unsigned char[w * h * c];
-        for ( int k = 0 ; k < c ; ++ k ) {
-            for ( int j = 0 ; j < h ; ++ j ) {
-                for ( int i = 0 ; i < w ; ++ i ) {
+        for (int k = 0; k < c; ++k) {
+            for (int j = 0; j < h; ++j) {
+                for (int i = 0; i < w; ++i) {
                     int destIdx = i + w * j + w * h * k;
                     int srcIdx = i + w * j + w * h * k;
                     // todo: gamma
-                    as < unsigned char >()[destIdx] = data[srcIdx];
+                    as<unsigned char>()[destIdx] = data[srcIdx];
                 }
             }
         }
@@ -53,17 +56,16 @@ Image::Image(const std::string & path, ImageLoadMode ilm) {
 
     resolution = Point2i(w, h);
     channels = c;
-
 }
 
-Image::Image(const Point2i & resolution, int channels) : resolution(resolution), channels(channels) {
+Image::Image(const Point2i &resolution, int channels) : resolution(resolution), channels(channels) {
     int w = resolution.x;
     int h = resolution.y;
     int c = channels;
     imageRawData = new unsigned char[w * h * c];
 }
 
-Image::Image(const Point3i & shape) : resolution(shape.x, shape.y), channels(shape.z) {
+Image::Image(const Point3i &shape) : resolution(shape.x, shape.y), channels(shape.z) {
     int w = resolution.x;
     int h = resolution.y;
     int c = channels;
@@ -71,29 +73,23 @@ Image::Image(const Point3i & shape) : resolution(shape.x, shape.y), channels(sha
     memset(imageRawData, 0, w * h * c * sizeof(unsigned char));
 }
 
-Point2i Image::getResolution() const
-{
+Point2i Image::getResolution() const {
     return resolution;
 }
 
-int Image::getChannels() const
-{
+int Image::getChannels() const {
     return channels;
 }
 
-int Image::getWidth() const
-{
+int Image::getWidth() const {
     return resolution.x;
 }
 
-int Image::getHeight() const
-{
+int Image::getHeight() const {
     return resolution.y;
 }
 
-
-
-void Image::setColorAt(const Point2i & p, const RGB3 & rgb) {
+void Image::setColorAt(const Point2i &p, const RGB3 &rgb) {
     int i = p.x;
     int j = p.y;
     int c = 3;
@@ -109,28 +105,26 @@ void Image::setColorAt(const Point2i & p, const RGB3 & rgb) {
     as<unsigned char>()[srcIdx + 2] = b;
 }
 
-RGB3 Image::getRGBColorAt(const Point2i & p) {
+RGB3 Image::getRGBColorAt(const Point2i &p) {
     RGB3 ans;
     int i = p.x;
     int j = p.y;
     int c = 3;
     int w = resolution.x;
     int srcIdx = c * i + c * w * j;
-    ans[0] = isHdr?as<float>()[srcIdx+0]:as<unsigned  char>()[srcIdx + 0] * 1. / 255;
-    ans[1] = isHdr?as<float>()[srcIdx+1]:as<unsigned  char>()[srcIdx + 1] * 1. / 255;
-    ans[2] = isHdr?as<float>()[srcIdx+2]:as<unsigned  char>()[srcIdx + 2] * 1. / 255;
+    ans[0] = isHdr ? as<float>()[srcIdx + 0] : as<unsigned char>()[srcIdx + 0] * 1. / 255;
+    ans[1] = isHdr ? as<float>()[srcIdx + 1] : as<unsigned char>()[srcIdx + 1] * 1. / 255;
+    ans[2] = isHdr ? as<float>()[srcIdx + 2] : as<unsigned char>()[srcIdx + 2] * 1. / 255;
     return ans;
 }
 
-Spectrum Image::getSpectrumColorAt(const Point2i &p)
-{
+Spectrum Image::getSpectrumColorAt(const Point2i &p) {
     return Spectrum(getRGBColorAt(p));
 }
 
-bool Image::saveTo(const std::string &path)
-{
-    auto destpath = FileUtils::getFilePath(path,"bmp",false);
-    const char * destBmpPath = destpath.c_str();
+bool Image::saveTo(const std::string &path) {
+    auto destpath = FileUtils::getFilePath(path, "bmp", false);
+    const char *destBmpPath = destpath.c_str();
     stbi_write_bmp(destBmpPath, resolution.x, resolution.y, 3, imageRawData);
     return true;
 }
